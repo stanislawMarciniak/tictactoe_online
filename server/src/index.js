88e532df277e59a3e50e3e66d181331be6928399
Stanislaw Marciniak <marciniak13ee@gmail.com>
2023-04-19 12:34:39 +0200

    hashing password

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,25 @@
 import express from "express";
 import cors from "cors";
+import { StreamChat } from "stream-chat";
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+const api_key = "d6ee4qzea2cu";
+const api_secret =
+  "azpwp44trbdv2g6qryrq4gtxwwuszppezc273xzjpkqeh5v5xtahgkeqqvr5sjpp";
+const serverClient = StreamChat.getInstance(api_key, api_secret);
+
+app.post("/signup", async (res, req) => {
+  const { firstName, lastName, username, password } = req.body;
+  const userId = uuidv4();
+  const hashedPassword = await bcrypt.hash(password, 10);
+});
+
+app.post("/login");
 
 app.listen(3001, () => {
   consol.log("Server is running on port 3001");
