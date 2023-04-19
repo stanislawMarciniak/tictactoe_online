5fea7be06eac611424b3b8f2692edf5ebddc6a0a
Stanislaw Marciniak <marciniak13ee@gmail.com>
2023-04-19 12:02:43 +0200

    login + signup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {};
+
+  return (
+    <div className="signUp">
+      <label>Login</label>
+      <input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={login}>Login</button>
+    </div>
+  );
 }
 
 export default Login;
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 function SignUp() {
   const [user, setUser] = useState(null);
 
+  const signUp = () => {};
+
   return (
     <div className="signUp">
       <label>Sign up</label>
@@ -10,6 +12,19 @@ function SignUp() {
         placeholder="Firts Name"
         onChange={(e) => setUser({ ...user, firstName: e.target.value })}
       />
+      <input
+        placeholder="Last Name"
+        onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+      />
+      <input
+        placeholder="Username"
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+      />
+      <input
+        placeholder="Password"
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <button onClick={signUp}>Sign up</button>
     </div>
   );
 }
