20cc127a8e0d42f506bc0298f78b7dbebb849c31
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-04-24 21:44:23 +0200

    api

--- server/src/index.js
+++ server/src/index.js
@@ -3,16 +3,17 @@ import cors from "cors";
 import { StreamChat } from "stream-chat";
 import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
-
+import * as dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-const api_key = "d6ee4qzea2cu";
-const api_secret =
-  "azpwp44trbdv2g6qryrq4gtxwwuszppezc273xzjpkqeh5v5xtahgkeqqvr5sjpp";
-// const api_key = process.env.REACT_APP_API_KEY;
-// const api_secret = process.env.REACT_APP_API_KEY_SECRET;
+// const api_key = "d6ee4qzea2cu";
+// const api_secret =
+//   "azpwp44trbdv2g6qryrq4gtxwwuszppezc273xzjpkqeh5v5xtahgkeqqvr5sjpp";
+const api_key = process.env.REACT_APP_API_KEY;
+const api_secret = process.env.REACT_APP_API_KEY_SECRET;
 const serverClient = StreamChat.getInstance(api_key, api_secret);
 
 app.post("/signup", async (req, res) => {
